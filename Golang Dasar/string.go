17036f14649de2d6d4238809976982917a9ed7b3
kyunkyuu <teguh.iqbal782@gmail.com>
2023-04-21 13:05:04 +0700

    string

---
+++ Golang Dasar/string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Satu")
+	fmt.Println("Dua")
+	fmt.Println("Tida koma lima")
+
+	fmt.Println(len("Satu"))
+	fmt.Println("Dua"[0])
+	fmt.Println("Tida koma lima"[1])
+}
